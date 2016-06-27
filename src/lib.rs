d6dca2065afb7ec43c84d88430cce66e71d88850
Sebastian Thiel <byronimo@gmail.com>
2016-06-27 10:03:27 +0200

    chore(bug): A compiler bug, it appears.
    This just makes no sense to me. On stable, it panics,
    on nightly, it aborts with something that appears to be
    a type-mismatch, for the wrong reason.

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,18 @@
 use std::path::{Path, PathBuf};
 use std::io;
 use std::env::current_dir;
-use std::fmt::Debug;
 
 /// Return the computed destination directory, given a source directory.
-pub fn destination_dir<P: AsRef<Path> + Debug>(source_dir: P, destination_dir: P) -> PathBuf {
+pub fn destination_dir<P: AsRef<Path>>(source_dir: P, destination_dir: P) -> PathBuf {
     let cwd = current_dir().unwrap();
     let source_dir = source_dir.as_ref();
-    print!("{:?} -> {:?} ({:?})", source_dir, destination_dir, cwd);
     destination_dir.as_ref()
-        .join(if source_dir.is_relative() {
-            source_dir
-                .file_name()
-                .unwrap_or(&cwd.file_name().expect("to not be at the root"))
-        } else {
-            "ROOT".as_ref()
-        })
+                   .join(if source_dir.is_relative() {
+                       source_dir.file_name()
+                                 .unwrap_or(&cwd.file_name().expect("to not be at the root"))
+                   } else {
+                       "ROOT".as_ref()
+                   })
 }
 
 pub fn copy_directory(source_dir: &Path, destination_dir: &Path) -> Result<PathBuf, io::Error> {
--- tests/copy.rs
+++ tests/copy.rs
@@ -5,35 +5,40 @@ extern crate tempdir;
 mod copy_directory {
     use tempdir::TempDir;
     use std::path::{Path, PathBuf};
-    use fs_utils::copy_directory;
+    use fs_utils::{destination_dir, copy_directory};
     fn fixture_at(name: &str) -> PathBuf {
         Path::new(file!()).parent().map(|p| p.join("fixtures").join(name)).unwrap()
     }
-    
+
     #[test]
-    fn it_copies_the_content_of_the_entire_directory_recursively_and_respects_basic_permissions() {
+    fn it_copies_the_content_of_the_entire_directory_recursively_and_respects_basic_permissions
+                                                                                                () {
         let dest = TempDir::new("dest").unwrap();
-        assert_eq!(copy_directory(&fixture_at("source-1"), dest.path()).unwrap(),
-        dest.path().join("source-1"))
+        let source = &fixture_at("source-1");
+        let dest_path = dest.path();
+        let copy_dest = destination_dir(source, dest.path());
+        assert_eq!(copy_directory(source, dest_path).unwrap(), copy_dest);
     }
 }
 
 mod destination_dir {
     use fs_utils::destination_dir;
     use std::path::PathBuf;
-    
+
     #[test]
     fn it_always_appends_the_filename_to_destination() {
-        assert_eq!(destination_dir("source/subdir", "dest"), PathBuf::from("dest/subdir"));
+        assert_eq!(destination_dir("source/subdir", "dest"),
+                   PathBuf::from("dest/subdir"));
     }
-    
+
     #[test]
     fn it_can_deal_with_the_root_directory() {
         assert_eq!(destination_dir("/", "dest"), PathBuf::from("dest/ROOT"))
     }
-    
+
     #[test]
     fn it_can_work_with_relative_paths() {
-        assert_eq!(destination_dir("", "dest"), PathBuf::from("dest/fs-utils"));
+        assert_eq!(destination_dir("", "dest"),
+                   PathBuf::from("dest/fs-utils-rs"));
     }
 }
